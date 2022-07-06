14bee4b1fab8c8bb71a669958ff19c5a2d18c201
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-07-07 00:23:47 +0900

    Add document for setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# mkdir -p ~/src/github.com/lambdasawa && git clone git@github.com:lambdasawa/lambdasawa.git ~/src/github.com/lambdasawa/lambdasawa && cd ~/src/github.com/lambdasawa/lambdasawa && ./setup.sh
+
 set -xeo pipefail
 
 for name in home-dirs home-bin symlinks essential asdf; do
