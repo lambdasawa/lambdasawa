05ce41f7782a23bae98fefbb71eafadfb8e8eceb
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-07-20 02:07:55 +0900

    Add sandbox/terraform-s3

---
+++ sandbox/terraform-s3/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "lambdasawa-sandbox-terraform-s3"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
