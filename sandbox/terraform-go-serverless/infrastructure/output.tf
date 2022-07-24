b1e9bad5152c7efbf885a7bb3fcaee8632f138b0
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-07-25 00:54:43 +0900

    Fix apigatewayv1_endpoint_main

--- sandbox/terraform-go-serverless/infrastructure/output.tf
+++ sandbox/terraform-go-serverless/infrastructure/output.tf
@@ -1,6 +1,11 @@
 output "apigatewayv1_endpoint_main" {
   description = "API Endpoint"
-  value       = aws_api_gateway_rest_api.main.id
+  value = format(
+    "https://%s.execute-api.%s.amazonaws.com/%s",
+    aws_api_gateway_rest_api.main.id,
+    local.region,
+    reverse(split("/", aws_api_gateway_stage.main.arn))[0]
+  )
 }
 
 output "apigatewayv2_endpoint_main" {
