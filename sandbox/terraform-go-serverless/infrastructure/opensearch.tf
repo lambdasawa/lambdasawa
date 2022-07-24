41caff9e96420102d5d02d198c0e668a6983ec86
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-07-24 20:37:19 +0900

    Add OpenSearch

---
+++ sandbox/terraform-go-serverless/infrastructure/opensearch.tf
@@ -0,0 +1,13 @@
+resource "aws_opensearch_domain" "main" {
+  domain_name    = "sandbox-tf-go-sl-main"
+  engine_version = "Elasticsearch_7.10"
+
+  cluster_config {
+    instance_type = "t3.small.search"
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+}
