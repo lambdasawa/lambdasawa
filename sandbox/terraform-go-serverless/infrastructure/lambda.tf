a8459cd5df2e0d24404b7fc29d5dc6d5982cdf24
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-07-24 20:01:45 +0900

    tweak

--- sandbox/terraform-go-serverless/infrastructure/lambda.tf
+++ sandbox/terraform-go-serverless/infrastructure/lambda.tf
@@ -1,3 +1,13 @@
+resource "aws_lambda_function" "main" {
+  function_name = "sandbox-terraform-go-serverless-main"
+
+  filename = data.archive_file.application.output_path
+  runtime  = "go1.x"
+  handler  = "./app"
+
+  role = aws_iam_role.iam_for_lambda_main.arn
+}
+
 data "archive_file" "application" {
   type        = "zip"
   source_file = "${path.module}/../application/bin/app"
@@ -22,12 +32,25 @@ resource "aws_iam_role" "iam_for_lambda_main" {
   })
 }
 
-resource "aws_lambda_function" "main" {
-  function_name = "sandbox-terraform-go-serverless-main"
+resource "aws_iam_policy" "iam_for_lambda_main" {
+  name = "sandbox-terraform-go-serverless-main"
 
-  filename = data.archive_file.application.output_path
-  runtime  = "go1.x"
-  handler  = "./app"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        Action : [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Effect : "Allow",
+        Resource : "arn:aws:logs:*:*:*"
+      }
+    ]
+  })
+}
 
-  role = aws_iam_role.iam_for_lambda_main.arn
+resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
+  role       = aws_iam_role.iam_for_lambda_main.id
+  policy_arn = aws_iam_policy.iam_for_lambda_main.arn
 }
