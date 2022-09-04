80122fd78050982c2c6b3988aa8814673e89a271
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-09-04 10:54:02 +0900

    amplify add api

--- sandbox/amplify-cfn-dyanmic-reference/react-amplified/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ sandbox/amplify-cfn-dyanmic-reference/react-amplified/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,17 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "test": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "test": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
