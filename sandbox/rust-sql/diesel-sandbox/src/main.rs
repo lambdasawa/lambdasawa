466b2503865b5e634c1d4711603a28a7bc5f01a7
Tsubasa Irisawa <lambdasawa@gmail.com>
2022-10-19 04:27:54 +0900

    Add CRUD sample

--- sandbox/rust-sql/diesel-sandbox/src/main.rs
+++ sandbox/rust-sql/diesel-sandbox/src/main.rs
@@ -1,3 +1,38 @@
+use diesel::prelude::*;
+use diesel_sandbox::models::*;
+use diesel_sandbox::*;
+
 fn main() {
-    println!("Hello, world!");
+    use self::schema::posts::dsl::*;
+    use diesel_sandbox::schema::posts;
+
+    let connection = &mut establish_connection();
+
+    let result = diesel::insert_into(posts::table)
+        .values(&NewPost {
+            title: "Hello",
+            body: "World",
+        })
+        .get_result::<Post>(connection)
+        .unwrap();
+    println!("INSERT {:?}", result);
+
+    let result = diesel::update(posts.find(result.id))
+        .set(published.eq(true))
+        .get_result::<Post>(connection)
+        .unwrap();
+    println!("UPDATE {:?}", result);
+
+    posts
+        .filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(connection)
+        .expect("Error loading posts")
+        .iter()
+        .for_each(|post| println!("SELECT {:?}", post));
+
+    let num_deleted = diesel::delete(posts.filter(title.like("%Hello%")))
+        .execute(connection)
+        .expect("Error deleting posts");
+    println!("DELETE {:?}", num_deleted);
 }
