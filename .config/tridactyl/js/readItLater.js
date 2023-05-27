a3d2f8da7cf02da306e258916fc5ea7b5519fcfc
Tsubasa Irisawa <lambdasawa@gmail.com>
2023-05-27 15:05:30 +0900

    Update tridactyl config

--- .config/tridactyl/js/readItLater.js
+++
@@ -1,7 +0,0 @@
-const title = encodeURIComponent(document.title);
-const body = encodeURIComponent(location.href);
-
-window.open(
-  `https://github.com/lambdasawa/lambdasawa/issues/new?title=${title}&body=${body}&assignee=lambdasawa&labels=read-it-later`,
-  "_blank"
-);
