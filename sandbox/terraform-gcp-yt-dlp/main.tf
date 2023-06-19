11d0120ba7494949bd78b6f6cf24b9d056288e0a
Tsubasa Irisawa <lambdasawa@gmail.com>
2023-06-19 15:11:46 +0900

    Shrink disk

--- sandbox/terraform-gcp-yt-dlp/main.tf
+++ sandbox/terraform-gcp-yt-dlp/main.tf
@@ -39,7 +39,7 @@ resource "google_compute_instance" "default" {
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-11"
-      size  = 2048
+      size  = 1000
       type  = "pd-ssd"
     }
   }
