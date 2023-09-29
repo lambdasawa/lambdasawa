99b2dad844e405895e77db1ccf3b6deb825c379b
Tsubasa Irisawa <lambdasawa@gmail.com>
2023-09-29 22:33:56 +0900

    Remove dotfiles

--- .config/tridactyl/js/addfeedly.js
+++
@@ -1,3 +0,0 @@
-const encodedURL = encodeURIComponent(location.href);
-const url = `https://feedly.com/i/discover/sources/search/feed/${encodedURL}`;
-window.open(url, "_blank");
--- .config/tridactyl/js/opentwitterintent.js
+++
@@ -1,3 +0,0 @@
-const text = encodeURIComponent(`${location.href} ${document.title}`);
-
-window.open(`https://twitter.com/intent/tweet?text=${text}`, "_blank");
